22679879866a3cf331984a0a18300c77e5125acb
mateocarruce <mateocarruce@gmail.com>
2025-02-10 14:57:33 -0500

    CI/CD: Build and deploy ReadUser to EC2 test 1

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ sequelize.sync().then(() => {
     console.log('✅ Database synced successfully!');
 
     server.listen({ port: 4006 }).then(({ url }) => {
-        console.log(`🚀 GraphQL server ready at ${url}`);
+        console.log(`🚀 GraphQL uwu server ready at ${url}`);
     });
 
     app.listen(5006, () => {
