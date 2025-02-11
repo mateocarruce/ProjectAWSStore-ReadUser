9fbdfef0fa3df5898953f990ee0047063016bd74
mateocarruce <mateocarruce@gmail.com>
2025-02-10 23:58:54 -0500

    Add serch by email and pasword for authentication test 1

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -4,13 +4,7 @@ const resolvers = {
     Query: {
         getAllUsers: async () => await User.findAll(),
         getUserById: async (_, { id }) => await User.findByPk(id),
-        getUserByEmail: async (_, { email }) => {
-            console.log(`🔍 Buscando usuario con email: ${email}`);
-            return await User.findOne({
-                where: { email },
-                attributes: ['id', 'email', 'password_hash']  // 🔥 Asegurar que incluya `password_hash`
-            });
-        }
+        getUserByEmail: async (_, { email }) => await User.findOne({ where: { email } }),
     },
 };
 
