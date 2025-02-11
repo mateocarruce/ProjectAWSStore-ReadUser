c4545ef530e50b0497f51f0e1a04e459c0f0af09
mateocarruce <mateocarruce@gmail.com>
2025-02-10 23:17:08 -0500

    Add serch by email

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -4,6 +4,7 @@ const resolvers = {
     Query: {
         getAllUsers: async () => await User.findAll(),
         getUserById: async (_, { id }) => await User.findByPk(id),
+        getUserByEmail: async (_, { email }) => await User.findOne({ where: { email } }),
     },
 };
 
